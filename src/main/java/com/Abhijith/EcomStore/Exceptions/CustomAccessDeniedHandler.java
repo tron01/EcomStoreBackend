1e6504ef083f63ec952a6a827ea278310bea7cc1
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-09 00:03:27 +0530

    security exceptions

--- src/main/java/com/Abhijith/EcomStore/Exceptions/CustomAccessDeniedHandler.java
+++ src/main/java/com/Abhijith/EcomStore/Exceptions/CustomAccessDeniedHandler.java
@@ -2,19 +2,27 @@ package com.Abhijith.EcomStore.Exceptions;
 
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.Column;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 			AccessDeniedException accessDeniedException) throws IOException, ServletException {
-		// TODO Auto-generated method stub
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		response.setContentType("application/json");
+		response.getWriter().write(objectMapper.writeValueAsString(accessDeniedException.getMessage()));
+		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 		
 	}
 
--- src/main/java/com/Abhijith/EcomStore/Exceptions/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/Abhijith/EcomStore/Exceptions/CustomAuthenticationEntryPoint.java
@@ -2,19 +2,26 @@ package com.Abhijith.EcomStore.Exceptions;
 
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.Column;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
-		// TODO Auto-generated method stub
+		ObjectMapper objectMapper = new ObjectMapper();
+		response.setContentType("application/json");
+		response.getWriter().write(objectMapper.writeValueAsString(authException.getMessage()));
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 		
 	}
 
