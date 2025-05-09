8401a12723fe02d5b65a992f953389467199f785
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-09 15:53:00 +0530

    validateToken ,extractUserId

--- src/main/java/com/Abhijith/EcomStore/Service/JwtService.java
+++ src/main/java/com/Abhijith/EcomStore/Service/JwtService.java
@@ -1,19 +1,57 @@
 package com.Abhijith.EcomStore.Service;
 
+import com.Abhijith.EcomStore.Model.Users;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
 
 @Service
 public class JwtService {
 	
+	private static final long EXPIRATION_TIME = 864000000; //- which equals to 10 da
+	private final String secretKey = "secre1qddwddqdqd41414f14fwffwffwfffwff1241444144qrtKeyfafqqtqrqwfwfmwfqfq2";
+	private final SecretKey signingKey = Keys.hmacShaKeyFor(secretKey.getBytes());
 	
-	public String generateToken(UserDetails user) {
-	
+	public String generateToken(Users user) {
+		return Jwts.builder()
+				 .subject(String.valueOf(user.getId()))
+				 .issuedAt(new Date())
+				 .expiration(new Date((new Date()).getTime() + EXPIRATION_TIME))
+				 .signWith(signingKey)
+				 .compact();
 	}
-
-	public String extractUsername(String token) {
+	
+	private Claims getClaimsFromToken(String token) {
+		return Jwts.parser()
+					   .verifyWith(signingKey)
+					   .build()
+					   .parseSignedClaims(token)
+					   .getPayload();
+	}
+	public String extractUserId(String token) {
+		return getClaimsFromToken(token).getSubject();
 	}
 
 	public boolean validateToken(String token, UserDetails user) {
+		String userIdFromToken = extractUserId(token);
+		if(user instanceof Users){
+			
+			long actualUserId= ((Users) user).getId();
+			
+			return userIdFromToken.equals(String.valueOf(actualUserId))
+						   && !isTokenExpired(token);
+		}
+		return false;
 	}
+	
+	private boolean isTokenExpired(String token) {
+		return getClaimsFromToken(token).getExpiration().before(new Date());
+	}
+	
 }
