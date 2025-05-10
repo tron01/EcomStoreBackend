e35732baf5aeebe42f10840ebc80dfcc5894713c
tron01 <69385258+tron01@users.noreply.github.com>
2025-05-10 13:47:13 +0530

    added cors

--- src/main/java/com/Abhijith/EcomStore/Config/SecurityConfig.java
+++ src/main/java/com/Abhijith/EcomStore/Config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,6 +21,11 @@ import com.Abhijith.EcomStore.Filter.JwtFilter;
 import com.Abhijith.EcomStore.Service.CustomUserDetailService;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -34,6 +40,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+				.cors(Customizer.withDefaults())
 			.exceptionHandling(e->e
 					.accessDeniedHandler(accessDeniedHandler)
 					.authenticationEntryPoint(authenticationEntrypoint))
@@ -63,6 +70,19 @@ public class SecurityConfig {
 		provider.setUserDetailsService(userDetailService);
 		provider.setPasswordEncoder(passwordEncoder());
 		return new ProviderManager(provider);
+	}
+	
+	//cors bean setup
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(List.of("http://localhost:5173"));
+		config.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
 	}
 	
 }
